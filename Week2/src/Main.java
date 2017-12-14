1f51b9573adc4771d599daad9529e3cf33a56a64
Justin <12138justin@gmail.com>
2017-12-14 10:53:14 +0000

    second

--- Week2/src/Main.java
+++ Week2/src/Main.java
@@ -1,27 +1,40 @@
 import java.awt.*;
+import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        String input = "";
-        ArrayList<Point2D.Float> room = null;
-        ArrayList<ArrayList<Point2D.Float>> objectsArray = null;
-        float current = 0;
-        float aim;
-        ArrayList<ArrayList<Point>> results;
+        String input = Input.INPUT;
+        ArrayList<Point2D.Double> room = new ArrayList<>();
+        ArrayList<ArrayList<Point2D.Double>> objectsArray = new ArrayList<ArrayList<Point2D.Double>>();
         Methods helper = new Methods();
         helper.parse(room, objectsArray, input);
-        aim = helper.findArea(room)/3 ;
-        while (current < aim) {
-            ArrayList<Point2D.Float> temp = helper.findBiggest(objectsArray);
-            //Point result = ifHaveRoom(element)
-            // if result != .... error value, add result to results
-            // if result == error value, get rid of result in the Objectarray
-            // current = area(result) + current;
-        }
-        // Find a way to print all the information from results
+        System.out.print(objectsArray);
+        ArrayList<ArrayList<Point2D.Double>> results = helper.ifHaveRoom(room,objectsArray);
+        String finalResults = "";
+        for (ArrayList<Point2D.Double> temp : results) {
 
+            for (int i = 0; i < temp.size();i++) {
+                if (i==temp.size()-1) {finalResults = finalResults + "("+ temp.get(i).getX()+", " + temp.get(i).getY()+")";}
+                else {finalResults = finalResults + "("+ temp.get(i).getX()+", " + temp.get(i).getY()+"), ";}
+            }
+            finalResults = finalResults + "; ";
+        }
+        //System.out.print(finalResults);
     }
 }
+
+/**
+ ArrayList<Point2D.Double> temp1 = new ArrayList<Point2D.Double>();
+ temp1.add(new Point2D.Double(0.0,0.0));
+ temp1.add(new Point2D.Double(1.0,1.0));
+ temp1.add(new Point2D.Double(2.0,2.0));
+ System.out.println(temp1);
+ ArrayList<Point2D.Double> newTransition = helper.trasition(temp1,1,1);
+ System.out.println(newTransition);
+
+ roomPolygon.contains(transitionValue)
+
+ **/
--- Week2/src/Methods.java
+++ Week2/src/Methods.java
@@ -1,81 +1,168 @@
 import java.awt.*;
+import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Methods {
-    // incomplete. Potentially many bugs and errors. But the idea should work
+    // parse is done. It is working.
 
-     void parse(ArrayList<Point2D.Float> room, ArrayList<ArrayList<Point2D.Float>> objectsArray, String str) {
-
+     void parse(ArrayList<Point2D.Double> room, ArrayList<ArrayList<Point2D.Double>> objectsArray, String str) {
+     // 1: (0, 0), (10, 0), (10, 10), (0, 10) # 9:(0, 0), (3, 0), (3, 3), (0, 3);4:(0, 0), (2, 0), (2, 2), (0, 2)
         String[] firstSplit = str.split("#");
         // firstSplit[0] = room, firstSplit [1] = objectsArray
         String[] secondSplit = firstSplit[1].split(";");
         // now secondSplit contains all the individual objects
-        for (String temp : secondSplit) {
-            StringBuilder sb = new StringBuilder(temp);
-            sb.deleteCharAt(0);
-            sb.deleteCharAt(1);
-            temp = sb.toString();
-        }
-        // now every string has no unit cost, easier to deal with
+        ArrayList<String> storing = new ArrayList<String>();
+         for ( String tempStr : secondSplit) {
+             tempStr = tempStr.replaceAll("[0-9]+:","");
+             storing.add(tempStr);
+         }
 
-        for (String temp : secondSplit) {
+       for (String temp : storing) {
             //(0, 0), (3, 0), (3, 3), (0, 3)
-            ArrayList<Point2D.Float> tempArray = new ArrayList<Point2D.Float>();
+            ArrayList<Point2D.Double> tempArray = new ArrayList<Point2D.Double>();
             tempArray = getPointArray(temp);
             objectsArray.add(tempArray);
         }
-        StringBuilder sb = new StringBuilder(firstSplit[0]);
-        sb.deleteCharAt(0);
-        sb.deleteCharAt(1);
-        room = getPointArray(sb.toString());
+
+        firstSplit[0] = firstSplit[0].replaceAll("[0-9]+:","");
+
+        room.addAll(getPointArray(firstSplit[0]));
+
         return;
     }
 
-     ArrayList<Point2D.Float> getPointArray(String str) {
-        // turning (0, 0), (3, 0), (3, 3), (0, 3) into ArrayList<Point2D.Float>
-        ArrayList<Point2D.Float> tempArray = new ArrayList<Point2D.Float>();
+     ArrayList<Point2D.Double> getPointArray(String str) {
+        // turning (0, 0), (3, 0), (3, 3), (0, 3) into ArrayList<Point2D.Double>
+        ArrayList<Point2D.Double> tempArray = new ArrayList<Point2D.Double>();
         char[] array = str.toCharArray();
         String temp = "";
-        for (char i : array) {
-            float x = -1;
-            float y = -1;
-            if (x != -1 && y != -1) {
-                Point2D.Float tempPoint = new Point2D.Float(x, y);
+        Double x = -1.0;
+        Double y = -1.0;
+        for ( int i = 0; i < array.length; i++) {
+            //int length = 0;
+            //if (length == array.length) {return tempArray;}
+            if (x != -1.0 && y != -1.0) {
+                Point2D.Double tempPoint = new Point2D.Double(x, y);
                 tempArray.add(tempPoint);
-                x = -1;
-                y = -1;
+                x = -1.0;
+                y = -1.0;
+                temp = "";
             }
+            if(array[i]=='-') { temp = temp + array[i];}
             if (Character.isDigit(array[i]) && Character.isDigit(array[i + 1])) {
-                temp = temp + i;
-            } else if (Character.isDigit(array[i]) && !Character.isDigit(array[i + 1])) {
-                temp = temp + 1;
-
-                if (x != -1) {
-                    y = Float.parseFloat(temp);
+                temp = temp + array[i];
+            } else if(Character.isDigit(array[i]) && array[i + 1]== '.' ){
+                temp = temp + array[i] + '.';
+            }
+            else if (Character.isDigit(array[i]) && ((array[i + 1])== ')'|| array[i + 1]== ',') ) {
+                temp = temp + array[i];
+                if (x == -1.0) {
+                    x = Double.parseDouble(temp);
+                    temp = "";
                 } else {
-                    x = Float.parseFloat(temp);
+                    y = Double.parseDouble(temp);
+                    temp = "";
                 }
             }
 
         }
-        return tempArray;
-    }
+     return tempArray;
+     }
 
-    float findArea(ArrayList<Point2D.Float> points) {
 
-        ArrayList<Point2D.Float> tempArray = points;
-        tempArray.add(points.get(0));
 
-        float sum = 0;
-        for (int i = 0; i < tempArray.size() - 1; ++i) {
-            sum += (tempArray.get(i).getX() * tempArray.get(i + 1).getY()) - (tempArray.get(i + 1).getX() * tempArray.get(i).getY());
+    ArrayList<ArrayList<Point2D.Double>> ifHaveRoom(ArrayList<Point2D.Double> room, ArrayList<ArrayList<Point2D.Double>> newElement) {
+        ArrayList<ArrayList<Point2D.Double>> tempNewElement = newElement;
+        ArrayList<ArrayList<Point2D.Double>> results = new ArrayList<ArrayList<Point2D.Double>>();
+        Path2D.Double roomPolygon = buildPolygon(room);
+        Rectangle2D Outerbound = roomPolygon.getBounds2D();
+        int xLow = Outerbound.OUT_LEFT;
+        int xHigh = Outerbound.OUT_RIGHT;
+        int yLow = Outerbound.OUT_BOTTOM;
+        int yHigh = Outerbound.OUT_TOP;
+        ArrayList<Point2D.Double> guesses = new ArrayList<Point2D.Double>();
+        int i = 0;
+
+        while (i < 10) {
+            // 1000 points inside the room
+            Random r = new Random();
+            double xVlaue = xLow + (xHigh - xLow) * r.nextDouble();
+            double yValue = yLow + (yHigh - yLow) * r.nextDouble();
+            Point2D.Double temp = new Point2D.Double(xVlaue,yValue);
+            if(roomPolygon.contains(temp) && !guesses.contains(temp)) {
+                guesses.add(temp);
+                i++;
+        }}
+
+        for (ArrayList<Point2D.Double> y : tempNewElement) {
+            // for each piece of furniture
+            for (Point2D.Double temp : guesses) {
+                // for each guess
+                ArrayList<Point2D.Double> newTransition = trasition(y,temp.getX(),temp.getY());
+                // we will make a new transition based on the guess -- moving this piece of furniture from 0,0 to guess
+                boolean q = true;
+                outer:
+                for (Point2D.Double transitionValue: newTransition) {
+                    // for each vertices in the new transition values
+                    if (roomPolygon.contains(transitionValue)) {
+                        // if it is in the room
+                        for (ArrayList<Point2D.Double> result : results) {
+                            // and if overlaps with other furniture
+                            if(buildPolygon(result).contains(transitionValue)) {
+                                q = false;
+                                break outer;
+                                // then we know it is not valid and we know this guessing point is not going to work
+                                // so we move on to another guessing point
+                            }
+                        }
+                    }
+                }
+                if (q==true) {
+                    results.add(newTransition);
+                }
+            }
         }
-        return Math.abs(sum / 2);
+        return results;
     }
 
-    ArrayList<Point2D.Float> findBiggest(ArrayList<ArrayList<Point2D.Float>> objectsArray) {
-        ArrayList<Point2D.Float> result;
+    ArrayList<Point2D.Double> trasition (ArrayList<Point2D.Double> previous, double x, double y) {
+         int length = previous.size();
+        ArrayList<Point2D.Double> newResult = new ArrayList<Point2D.Double>();
+         for (int i = 0; i < length ; i++) {
+             Point2D.Double temp = new Point2D.Double(previous.get(i).getX() + x, previous.get(i).getY() + y );
+             newResult.add(temp);
+         }
+        return newResult;
+    }
+
+    Path2D.Double buildPolygon (ArrayList<Point2D.Double> room) {
+        Path2D.Double path = new Path2D.Double();
+        boolean isFirst = true;
+        double firstTimeX = 0.0;
+        double firstTimeY = 0.0;
+        for (Point2D.Double i : room) {
+             if (isFirst) {
+                 path.moveTo(i.getX(), i.getY());
+                 firstTimeX = i.getX();
+                 firstTimeY = i.getY();
+                 isFirst = false;
+             } else {
+                 path.lineTo(i.getX(), i.getY());
+             }
+         }
+         //path.lineTo(firstTimeX,firstTimeY);
+         path.closePath();
+         return path;
+    }
+
+
+  /*  ArrayList<Point2D.Double> findBiggest(ArrayList<ArrayList<Point2D.Double>> objectsArray) {
+        ArrayList<Point2D.Double> result;
 
         result = objectsArray.get(0);
         for (int i = 1; i < objectsArray.size() - 1; i++) {
@@ -84,19 +171,18 @@ public class Methods {
             }
         }
         return result;
-    }
+} */
+  /* float findArea(ArrayList<Point2D.Double> points) {
 
-    // Still trying to implement this method
-    ArrayList<Point2D.Float> ifHaveRoom(ArrayList<Point2D.Float> room, ArrayList<ArrayList<Point2D.Float>> results,
-                                        ArrayList<Point2D.Float> newElement) {
+        ArrayList<Point2D.Double> tempArray = points;
+        tempArray.add(points.get(0));
 
-         // This is the hardest one. Still not done yet
-
-
-
-    }
-
-
+        float sum = 0;
+        for (int i = 0; i < tempArray.size() - 1; ++i) {
+            sum += (tempArray.get(i).getX() * tempArray.get(i + 1).getY()) - (tempArray.get(i + 1).getX() * tempArray.get(i).getY());
+        }
+        return Math.abs(sum / 2);}
+   */
 }
 
 
